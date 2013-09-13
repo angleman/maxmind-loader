f4add5a1dd110b8fea0c3f2dc2b951bd594040e0
angleman <angleman@cormea.com>
2013-09-13 17:07:51 -0600

    expand tests

--- test/_general.js
+++ test/_general.js
@@ -1,27 +1,33 @@
 var should    = require('should')
-  , maxloader = require('../maxmind-loader.js')
   , fs        = require('fs')
   , geofile   = '/tmp/GeoLiteCity.dat'
   , geofilegz = geofile + '.gz'
 ;
 
 
-// clean up from prior run
-if (fs.existsSync(geofile)) {
-    fs.unlinkSync(geofile);
-}
-if (fs.existsSync(geofilegz)) {
-    fs.unlinkSync(geofilegz);
-}
-
 
 describe('maxmind-loader', function() {
 
+    // clean up from prior run
+    if (fs.existsSync(geofile)) {
+        fs.unlinkSync(geofile);
+    }
+    if (fs.existsSync(geofilegz)) {
+        fs.unlinkSync(geofilegz);
+    }
+
+    var exists;
+    var maxloader;
+
     describe('cleanup test data', function() {
-      it(geofile + " and " + geofilegz + "should not exist", function(){  
-        var exists = (fs.existsSync(geofile));
-        exists.should.not.be.equal(true);
+
+      it(geofilegz + " should not exist", function(){  
         exists = (fs.existsSync(geofilegz));
+        exists.should.not.be.equal(true);
+      }); 
+
+      it(geofile + " should not exist", function(){  
+        exists = (fs.existsSync(geofile));
         exists.should.not.be.equal(true);
       }); 
     });
@@ -34,6 +40,8 @@ describe('maxmind-loader', function() {
         var size, fstat;
         beforeEach(function(done){
             this.timeout(15 * 60 * 1000); // allow test to run for 15 minutes
+            maxloader = require('../maxmind-loader.js');
+
             maxloader({
                 dest: '/tmp/'
             }, function(err) {
@@ -47,12 +55,22 @@ describe('maxmind-loader', function() {
             flag.should.equal(true);  
         }); 
 
+        it(geofilegz + " exist", function(){
+            flag = fs.existsSync(geofilegz);  
+            flag.should.equal(true);  
+        }); 
+
         it(geofilegz + " be above 9MB", function(){
             flag = fs.existsSync(geofilegz);  
             flag.should.equal(true);  
             fstat = fs.statSync(geofilegz);
             size  = fstat.size;
             size.should.be.above(9001000); // at least 9MB, assumes maxmind doesn't shrink the free data set much
+        }); 
+
+        it(geofile + " exist", function(){  
+            flag = fs.existsSync(geofile);  
+            flag.should.equal(true);  
         }); 
 
         it(geofile + " be above 14MB", function(){  
